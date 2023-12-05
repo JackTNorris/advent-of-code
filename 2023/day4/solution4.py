dfc62f2a1014e95bd6ac752e897328fa1e815307
Jack Norris <jacktimothynorris@gmail.com>
2023-12-04 20:11:15 -0600

    part2 day 4

--- 2023/day4/solution4.py
+++ 2023/day4/solution4.py
@@ -9,23 +9,42 @@ def return_array_from_file(filename):
         return list(map(lambda item_set: item_set.split('\n'), line_array))
 
 def extract_winning_mine_array(row):
-    nums = row.split(':')[1]
+    [cardNum, nums] = row.split(':')
+    cardNum = int(cardNum.replace('Card', '').strip())
     nums =  list(map(lambda x: list(filter(lambda y: y != '', x.strip().split(' '))), nums.split('|')))
-    return nums
-    
+    return (cardNum, nums)
 
 def part1():
     data = return_array_from_file('part-1-input.txt')[0]
     points = 0
     for row in data:
         row_point = 0
-        [win, mine] = extract_winning_mine_array(row)
+        [cardNum, nums] = extract_winning_mine_array(row)
+        [win, mine] = nums
         for num in mine:
             if num in win:
                 row_point = 1 if row_point == 0 else row_point * 2
         points += row_point
     print(points)
-          
+
+def part2():
+    inventory = [1]*193
+    print(inventory)
+    data = return_array_from_file('part-2-input.txt')[0]
+    for i in range(len(data)):
+        row = data[i]
+        [cardNum, nums] = extract_winning_mine_array(row)
+        [win, mine] = nums
+        num_match = 0
+        for num in mine:
+            if num in win:
+                num_match += 1
+        
+        for j in range(num_match):
+            k = i + j + 1
+            for z in range(inventory[i]):
+                inventory[k] = inventory[k] + 1
+    print(sum(inventory)) 
 
 if __name__ == "__main__":
-    part1()
+    part2()
