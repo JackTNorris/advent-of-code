88097b8cb954833fd793fd9bb8a5be544015c833
Jack Norris <jacktimothynorris@gmail.com>
2024-12-23 18:51:57 -0300

    completed day 20 part 2 with dijkstra & manhattan

--- 2024/day20/solution20.py
+++ 2024/day20/solution20.py
@@ -42,31 +42,83 @@ def part1():
             if track[row][col] == 'S':
                 temp.append(0)
                 start_pos = (row, col)
+                walkable.append((row, col))
             elif track[row][col] == 'E':
                 walkable.append((row, col))
                 temp.append(float('inf'))
                 end_pos = (row, col)
             else:
-                if track[row][col] == '.':
+                if track[row][col] != '#':
                     walkable.append((row, col))
                 temp.append(float('inf'))
         dijkstra_navigator.append(temp)
-    dijkstra_navigator_copy = deepcopy(dijkstra_navigator)
 
     navigate_dijkstra(start_pos, dijkstra_navigator, track)
-    track_traverse_time = dijkstra_navigator[end_pos[0]][end_pos[1]]
     res = 0
     all_dirs = [(-2, 0), (2, 0), (0,- 2), (0, 2), (-1, -1), (1, 1), (-1, 1), (1, -1)]
     for walk in walkable:
-        to_check = []
         for d in all_dirs:
             temp = (walk[0] + d[0], walk[1] + d[1])
-            if temp[0] >= 0 and temp[0] < len(track[1]) and temp[1] >= 0 and temp[1] < len(track[0]):
+            if temp[0] >= 0 and temp[0] < len(track) and temp[1] >= 0 and temp[1] < len(track[0]):
                 if track[temp[0]][temp[1]] == '.' or track[temp[0]][temp[1]] == 'E' :
                     if dijkstra_navigator[temp[0]][temp[1]] - dijkstra_navigator[walk[0]][walk[1]] >= save_me + 2:
                         res += 1
     return res
     
+def manhattan_vectors(max_distance=20):
+    vectors = []
+    for x in range(-max_distance, max_distance + 1):
+        for y in range(-max_distance, max_distance + 1):
+                if abs(x) + abs(y) <= max_distance:
+                    vectors.append((x, y))
+    return vectors
+
+def man_distance(vector):
+    return abs(vector[0]) + abs(vector[1])
+
+def part2():
+    save_me = 100
+    track = list(map(lambda x: list(x), aoc_utils.return_array_from_file('input.txt')[0]))
+    start_pos, end_pos = None, None
+    dijkstra_navigator = []
+    walkable = []
+    for row in range(len(track)):
+        temp = []
+        for col in range(len(track[0])):
+            if track[row][col] == 'S':
+                temp.append(0)
+                start_pos = (row, col)
+                walkable.append((row, col))
+            elif track[row][col] == 'E':
+                walkable.append((row, col))
+                temp.append(float('inf'))
+                end_pos = (row, col)
+            else:
+                if track[row][col] != '#':
+                    walkable.append((row, col))
+                temp.append(float('inf'))
+        dijkstra_navigator.append(temp)
+
+    my_temp = []
+    navigate_dijkstra(start_pos, dijkstra_navigator, track)
+    res = 0
+    all_dirs = manhattan_vectors(20)
+    for walk in walkable:
+        for d in all_dirs:
+            temp = (walk[0] + d[0], walk[1] + d[1])
+            if temp[0] >= 0 and temp[0] < len(track) and temp[1] >= 0 and temp[1] < len(track[0]):
+                if track[temp[0]][temp[1]] != '#':
+                    if dijkstra_navigator[temp[0]][temp[1]] - dijkstra_navigator[walk[0]][walk[1]] >= save_me + man_distance(d):
+                        my_temp.append(dijkstra_navigator[temp[0]][temp[1]] - dijkstra_navigator[walk[0]][walk[1]] - man_distance(d))
+                        res += 1
+    my_temp.sort()
+    g = defaultdict(lambda: 0)
+    for item in my_temp:
+        g[item] += 1
+    for item in g.keys():
+        print("Number of: " + str(item) + ": ", g[item])
+    return res
+    
 
 
-print(part1())
+print(part2())
