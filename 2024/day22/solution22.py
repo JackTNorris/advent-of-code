c8b7f3b90e26bcff7f05700079b6234451c4155f
Jack Norris <jacktimothynorris@gmail.com>
2024-12-25 14:08:35 -0300

    added in part 2 of day 22

--- 2024/day22/solution22.py
+++ 2024/day22/solution22.py
@@ -33,4 +33,40 @@ def part1():
         res += secret
     return res
 
-print(part1())
+def part2():
+    data = aoc_utils.return_array_from_file('input.txt')[0]
+    res = 0
+    buyer_price_changes = []
+    selling_prices = []
+    for item in data:
+        price_changes = []
+        sp = []
+        secret = int(item)
+        for i in range(2000):
+            sp.append(int(str(secret)[-1]))
+            temp = secret
+            secret = get_next_secret_number(secret)
+            price_changes.append(int(str(secret)[-1]) - int(str(temp)[-1]))
+        buyer_price_changes.append(price_changes)
+        selling_prices.append(sp)
+
+    buyer_maps = [defaultdict(lambda: 0) for i in range(len(data))]
+    all_possibilities = set()
+    for buyer_index in range(len(buyer_price_changes)):
+        for z in range(len(buyer_price_changes[buyer_index]) - 4):
+            to_match = buyer_price_changes[buyer_index][z:z+4]
+            price = selling_prices[buyer_index][z + 4]
+            if tuple(to_match) not in buyer_maps[buyer_index]:
+                buyer_maps[buyer_index][tuple(to_match)] = price
+                all_possibilities.add(tuple(to_match))
+    print('made buyer map')
+    res = 0
+    for poss in all_possibilities:
+        sum = 0
+        for i in range(len(buyer_maps)):
+            sum += buyer_maps[i][poss]
+        res = max(res, sum)
+    return res
+
+
+print("Part 2: ", part2())
