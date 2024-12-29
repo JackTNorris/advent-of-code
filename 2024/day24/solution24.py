b4b8ff9f6ef9743fd01bedaabad673d2f48a61f8
Jack Norris <jacktimothynorris@gmail.com>
2024-12-28 23:39:31 -0300

    day24, part 2, want to come up with programmatic solution because had to do some manual checks

--- 2024/day24/solution24.py
+++ 2024/day24/solution24.py
@@ -48,4 +48,76 @@ def part1():
         dec_rep += known_values[z_outputs[i]] * 2**i
     return dec_rep
 
-print(part1())
+def part2():
+    # know we're dealing with a ripple carry adder based on reddit + some help
+    # diagram here: https://media.geeksforgeeks.org/wp-content/uploads/3-57.png
+
+    start_values, connections = aoc_utils.return_array_from_file('input.txt')
+    known_values = {}
+    rely_on = defaultdict(lambda: [])
+    for item in start_values:
+        name, val = item.split(': ')
+        known_values[name] = int(val)
+    
+    # maps all "wires" to the expressions it is a part of
+    conn_dict = defaultdict(lambda: set())
+
+    for conn in connections:
+        expression, assignment = conn.split(' -> ')
+        n1, op, n2 = expression.split(' ')
+        conn_dict[n1].add(conn)
+        conn_dict[n2].add(conn)
+        if assignment.find('z') > -1 and expression.find('XOR') < 0:
+            print('problem with ', assignment)
+
+        
+    
+    # num input outputs
+    carry_in = None
+    carry_out = None
+
+    
+    for i in range(45):
+        # test x y interaction
+        x_input = "x{:02d}".format(i)
+        y_input = "y{:02d}".format(i)
+        xys = set(conn_dict[x_input] & conn_dict[y_input])
+        if len(xys) != 2:
+            print('fishiness with x', i)
+        first_xor_output = None
+        first_and_output = None
+        for conn in xys:
+            if conn.find("XOR") > -1:
+                expression, assignment = conn.split(' -> ')
+                known_values[assignment] = evaluate(expression, known_values)
+                first_xor_output = assignment
+            if conn.find("AND") > -1:
+                expression, assignment = conn.split(' -> ')
+                known_values[assignment] = evaluate(expression, known_values)
+                first_and_output = assignment
+            known_values[assignment] = evaluate(expression, known_values)
+        if first_xor_output == None or first_and_output == None:
+            # checked, no issues here
+            print('Problem with x or y: ',  i)
+        
+        # test that the AND output of XY is ORD'd and only used once:
+        temp = conn_dict[first_and_output]
+        if len(temp) > 1:
+            print("Something is possibly wrong with: ", first_and_output)
+        
+        
+        # test that the XOR output of XY is somehow XOR'd into Zi (one time)
+        temp = list(filter(lambda x: x.find('XOR') >= 0 and x.find("z{:02d}".format(i)) >= 0, conn_dict[first_xor_output]))
+        if len(temp) != 1:
+            print("Something is possibly wrong with: ", first_xor_output)
+        """
+        # test that the XOR output of XY is AND'd with something (only once):
+        temp = list(filter(lambda x: x.find('AND') >= 0, conn_dict[first_xor_output]))
+        if len(temp) != 1:
+            print("Something is possibly wrong with: ", first_xor_output)
+        """
+        
+
+        
+
+print("Part 2: ", part2())
