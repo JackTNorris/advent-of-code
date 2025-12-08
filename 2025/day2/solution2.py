20060e73102b5956a58b8a53e1c001cdac44835c
Jack Norris <jacktimothynorris@gmail.com>
2025-12-07 22:08:13 -0300

    knocked out day 2 2025

--- 2025/day2/solution2.py
+++ 2025/day2/solution2.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python3
-
-from pathlib import Path
-
-
-def part1(data: str):
-    # TODO: implement Day 2 part 1
-    return None
+import sys
+sys.path.append('../../')
+import aoc_utils
+from typing import List
 
 
-def part2(data: str):
-    # TODO: implement Day 2 part 2
-    return None
+def part1(data: List[str]):
+    res = 0
+    for case in data:
+        min_val, max_val = map(int, case.split('-'))
+        if len(str(min_val)) % 2 == 1 and len(str(max_val)) % 2 == 1:
+            res += 0
+        else:
+            for i in range(min_val, max_val + 1):
+                str_num = str(i)
+                if len(str_num) % 2 == 0 and str_num[0:len(str_num)//2] == str_num[len(str_num)//2:]:
+                    res += i
+    return res
+
+
+def part2(data: List[str]):
+    res = 0
+    def is_valid(s):
+        # sliding window i
+        for i in range(1, len(s) // 2 + 1):
+            is_good = True
+            to_match = s[0:i]
+            if len(s) % len(to_match) == 0:
+                for j in range(int(len(s) / len(to_match))):
+                    if s[j*len(to_match):(j+1)*len(to_match)] != to_match:
+                        is_good = False
+                        break
+            else:
+                is_good = False
+            if is_good:
+                return True
+    for case in data:
+        min_val, max_val = map(int, case.split('-'))
+        for i in range(min_val, max_val + 1):
+            str_num = str(i)
+            if is_valid(str_num):
+                res += i
+    return res
 
 
 if __name__ == "__main__":
-    data = Path("input.txt").read_text().rstrip("\n")
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    data = aoc_utils.return_array_from_file('input.txt')
+    #print("".join(data[0]).split(','))
+    print("Part 1: ", part1("".join(data[0]).split(',')))
+    print("Part 2: ", part2("".join(data[0]).split(',')))
