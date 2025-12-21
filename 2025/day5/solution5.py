d50877b50da99d93dbec6afaa13927960bcdba8a
Jack Norris <jacktimothynorris@gmail.com>
2025-12-21 18:19:32 -0300

    day 5 part 1

--- 2025/day5/solution5.py
+++ 2025/day5/solution5.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
+import sys
+sys.path.append('../../')
+import aoc_utils
+from typing import List
+from collections import defaultdict
 
+# hacky as crap, but oiko
+def part1(ranges, ids):
+    res = 0
+    for id in ids:
+        int_id = int(id)
+        for r in ranges:
+            r1, r2 = list(map(int, r.split('-')))
+            r2 -= int_id
+            temp_int_id = int_id - r1
+            if temp_int_id >= 0 and r2 >= 0:
+                res += 1
+                break
+    return res
+    
+    
 
-def part1(data: str):
-    # TODO: implement Day 5 part 1
-    return None
+# looking to make the biggest 12 digit number from the input
+def part2(ranges, ids):
+    res = 0
 
-
-def part2(data: str):
-    # TODO: implement Day 5 part 2
-    return None
-
-
+    return res
 if __name__ == "__main__":
-    data = Path("input.txt").read_text().rstrip("\n")
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    data = aoc_utils.return_array_from_file('input.txt')
+
+    print("Part 1: ", part1(data[0], data[1]))
+    print("Part 2: ", part2(data[0], data[1]))
