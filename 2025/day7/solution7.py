c831fe9b905987abacb6c8bcb4f0614831087ab5
Jack Norris <jacktimothynorris@gmail.com>
2026-01-05 21:26:53 -0300

    part 1 day 7

--- 2025/day7/solution7.py
+++ 2025/day7/solution7.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python3
+from typing import List
+import sys
+sys.path.append('../../')
+import aoc_utils
+from typing import List
+from collections import defaultdict
 
-from pathlib import Path
+# no such thing as ^^, ^ aren't on edges either
+def part1(data):
+    res = 0
+    num_rows, num_cols = len(data), len(data[0])
+    print(num_rows)
+    print(num_cols)
+    data = list(map(list, data))
+    for i in range(1, num_rows):
+        above_row = data[i-1]
+        for j in range(num_cols):
+            if data[i][j] == '^':
+                if above_row[j] == '|':
+                    data[i][j - 1] = '|'
+                    data[i][j + 1] = '|'
+                    res += 1
+            elif above_row[j] == 'S':
+                data[i][j] = '|'
+            elif above_row[j] == '|':
+                data[i][j] = '|'
+    return res
 
+    
 
-def part1(data: str):
-    # TODO: implement Day 7 part 1
-    return None
-
-
-def part2(data: str):
-    # TODO: implement Day 7 part 2
-    return None
-
-
+def part2(data: List[str]):
+    num_rows, num_cols = len(data), len(data[0])
+    data = list(map(list, data))
+    for i in range(1, num_rows):
+        above_row = data[i-1]
+        for j in range(num_cols):
+            if data[i][j] == '^':
+                if above_row[j] == '|':
+                    data[i][j - 1] = '|'
+                    data[i][j + 1] = '|'
+            elif above_row[j] == 'S':
+                data[i][j] = '|'
+            elif above_row[j] == '|':
+                data[i][j] = '|'
+    for row in data:
+        print(row)
 if __name__ == "__main__":
-    data = Path("input.txt").read_text().rstrip("\n")
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    data = aoc_utils.return_array_from_file('input.txt')
+    #print("Part 1: ", part1(data[0]))
+    part2(data[0])
