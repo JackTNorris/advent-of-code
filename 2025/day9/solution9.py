3c3d3713e5925f2f51a939380be1103fc3c1acc6
Jack Norris <jacktimothynorris@gmail.com>
2026-01-09 23:22:50 -0300

    deleted unused folders

--- 2025/day9/solution9.py
+++ 2025/day9/solution9.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
+from typing import List
+import sys
+sys.path.append('../../')
+import aoc_utils
+from typing import List
+from collections import defaultdict
+import math
 
-from pathlib import Path
 
+def calc_rect_area(p1, p2):
+    return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
 
-def part1(data: str):
-    # TODO: implement Day 9 part 1
-    return None
+def part1(data):
+    res = 0
+    for i in range(len(data)):
+        [x1, y1] = map(int, data[i].split(','))
+        for j in range(i + 1, len(data)):
+            [x2, y2] = map(int, data[j].split(','))
+            res = max(res, calc_rect_area((x1, y1), (x2, y2)))
+    return res
 
+    
 
-def part2(data: str):
-    # TODO: implement Day 9 part 2
-    return None
-
+def part2(data: List[str]):
+    return 0
+    
 
 if __name__ == "__main__":
-    data = Path("input.txt").read_text().rstrip("\n")
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    data = aoc_utils.return_array_from_file('input.txt')
+    print("Part 1: ", part1(data[0]))
+    #print("Part 2: ", part2(data[0]))
